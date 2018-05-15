01013053d7ffd32117514c6e8aa608807131f485
harrison8024 <harrison8024@gmail.com>
2018-05-14 17:30:11 -0700

    Completed adding new to do items

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import List from './list';
+import {Route} from 'react-router-dom';
 import 'materialize-css/dist/css/materialize.min.css';
+import AddItem from './add_item';
 
 const App = () => (
     <div className="container">
         <h1 className="center">TO DO LIST</h1>
-        <List/>
+        <Route exact path="/" component={List}/>
+        <Route path="/add-item" component={AddItem}/>
     </div>
 );
 
